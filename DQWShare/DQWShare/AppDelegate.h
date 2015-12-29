86f5930e75a579f3d6238660ec633b28fc751aab
dqw <dqwdlut@qq.com>
2015-12-29 18:00:19 +0800

    initialize

---
+++ DQWShare/DQWShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DQWShare
+//
+//  Created by dqw on 15/12/29.
+//  Copyright (c) 2015年 dqw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DQWShare/DQWShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DQWShare
+//
+//  Created by dqw on 15/12/29.
+//  Copyright (c) 2015年 dqw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
